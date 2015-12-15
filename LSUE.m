60607304f800a0b582ae607a4e9a52845fb926eb
ekffar <371795633@qq.com>
2015-12-15 20:15:50 +0800

    LSUE

--- LSUE.m
+++ LSUE.m
@@ -1,29 +1,37 @@
-function [value] = LSUE( tr_cell,tr_TF, B, Lamda, te_cell, te_TF )
+function [value] = LSUE( tr_cell,tr_TF, O, Group_Index, Lamda, te_Cell, te_TF )
 
+% 
 
-
-[~,~,C_ID] = unique(tr_cell);
+[Cell,~,C_ID] = unique(tr_cell);
 clear cell
-[~,~,T_ID] = unique(tr_TF);
+[TF,~,T_ID] = unique(tr_TF);
 clear TF
 
-c_num = max(C_ID);
+ng = length(Group_Index);
+np = size(O,2);
+
+nc = max(C_ID);
+nm = max(T_ID);
 
 A = zeros(length(cell),max(C_ID)+max(T_ID));
 
 for i = 1:size(A,1)
     A(i, C_ID(i)) = 1;
-    A(i, c_num+T_ID(i)) = 1;
+    A(i, nc+T_ID(i)) = 1;
 end
 
-l = size(B,2);
+tmp = strcmp(te_Cell,Cell);
+index_c = tmp>0;
 
-Num = 4 ;
+tmp = strcmp(te_TF,TF);
+index_t = find(tmp>0);
 
-Interval = ceil(l/Num);
+te_A = zeros(1,size(A,2));
+te_A(index_c) = 1;
+te_A(index_t+nc) = 1;
 
-Z_Index = cell(1,1);
-Z = cell(1,1);
+l = size(O,2);
+
 
 [U1,D1,V1] = svd(A,'econ');
 D1 = diag(D1);
@@ -33,56 +41,47 @@ V1 = V1(:,D1>max(D1)*1e-6);
 D1 = D1(D1>max(D1)*1e-6);
 
 
-B = U1'*B;
+O = U1'*O;
 
-results = [];
-T = B*B';
+T = O*O';
 [P,D] = eig(T);
 
 D = diag(D);
 
 index = D>mean(D)*1e-6;
-
 P = P(:,index);
+O = P'*O;
+W_L = te_A*V1*diag(1./D1)*P;
 
-B = P'*B;
+Z = cell(1,ng);
 
-W_L = A1*V1*diag(1./D1)*P;
-
-
-
-for i = 1:3
-    Z_Index{i} = [(i-1)*Interval+1:min((i+1)*Interval,l)];
-    Z{i} = zeros(size(B,1),length(Z_Index{(i+1)/2}));
+for i = 1:ng
+    Z{i} = zeros(size(O,1),length(Group_Index{i}));
 end
 
 W_A = zeros(1,l);
-for i = 1:length(Z_Index)
-    W_A(Z_Index{i}) = W_A(Z_Index{i}) + 1;
+for i = 1:length(Group_Index)
+    W_A(Group_Index{i}) = W_A(Group_Index{i}) + 1;
 end
 
 
 Y = Z;
 
-mu = 2e-2;
-rol = 1.25;
+mu = 1e-2;
+rol = 1.1;
 
 iter = 30;
-
+% Compute X using ADMM
 for i = 1:iter
-    tic
-    T = repmat(1./(1 + mu*W_A/2),size(B,1),1);
-    X = 2*B;
-    for j = 1:length(Z_Index)
-        X(:,Z_Index{j}) = X(:,Z_Index{j}) - Y{j} + mu*Z{j}; 
+    T = repmat(1./(1 + mu*W_A/2),size(O,1),1);
+    X = 2*O;
+    for j = 1:length(Group_Index)
+        X(:,Group_Index{j}) = X(:,Group_Index{j}) - Y{j} + mu*Z{j}; 
     end
     X = T.*X/2;
     
-    rank = 0;
-    
-    for j = 1:length(Z_Index)
-        T = X(:,Z_Index{j}) + Y{j}/mu;
-%         size(T)
+    for j = 1:length(Group_Index)
+        T = X(:,Group_Index{j}) + Y{j}/mu;
         T1 = T*T';
         [U,D] = eig((T1+T1')/2);
         D = diag(D);
@@ -92,7 +91,6 @@ for i = 1:iter
         D(D<0) = 0;
         U = U(:,D>0);
         D = D(D>0);
-        rank = rank + length(D);
         V = T'*U;
         V = normc(V);
         Z{j} = U*diag(D)*V';
@@ -100,12 +98,49 @@ for i = 1:iter
     
     clear T V U
     
-    for j = 1:length(Z_Index)
-        Y{j} = Y{j} + mu*(X(:,Z_Index{j})-Z{j});
-    end
-        
+    for j = 1:length(Group_Index)
+        Y{j} = Y{j} + mu*(X(:,Group_Index{j})-Z{j});
+    end        
     mu = mu*rol;
-    
-    value = W_L*X; 
 end
+
+% Compute B using ADMM
+mu = 1e-2;
+rol = 1.1;
+iter = 30;
+
+Oe = mean(O,2);
+
+B = zeros(nm,nc);
+B1 = zeros(nm,nc);
+Z = zeros(nm,nc);
+
+OB = B;
+
+Omega = zeros(nm,nc);
+
+for i = 1:size(A,1)
+    Omega(T_ID(i),C_ID(i)) = 1;
+    OB(T_ID(i),C_ID(i))) = Oe(i);
+end
+
+for i = 1:iter
+    B(Omega==0) = B1(Omega==0) - Z(Omega==0)/mu;
+    B(Omega==1) = (B(Omega==1)*mu/2 + OB(Omega==1) - Z(Omega==1)/2)/(1+mu/2);
+    
+    B1 = B + Z/mu;
+    
+    [U,D,V] = svd(B1,'econ');
+    D = diag(D);
+    D = D - Lamda/mu;
+    D(D<0) = 0;
+    B1 = U*diag(D)*V';
+    
+    Z = Z + mu*(B-B1);
+    
+    mu = mu*rol;
+end
+
+value = W_L*X + B(index_t,index_c); 
+
 
